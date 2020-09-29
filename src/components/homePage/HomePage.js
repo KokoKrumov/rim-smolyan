d3cc0d0208acecb2120db3af84bf157f6b661b2b
Kaloyan Krumov <enzerbilen@gmail.com>
2020-09-29 09:15:51 +0300

    add link and effects in nae container

--- src/components/homePage/HomePage.js
+++ src/components/homePage/HomePage.js
@@ -4,6 +4,7 @@ import Hero from "../hero/Hero";
 import WorkInfoLine from "../workInfoLine/WorkInfoLine"
 import NewsAndEventsList from "../newsAndEventsList/NewsAndEventsList";
 import Container from "react-bootstrap/cjs/Container";
+import {Link} from "react-router-dom";
 
 class HomePage extends Component {
 
@@ -26,14 +27,44 @@ class HomePage extends Component {
                                     Новини и Събития
                                 </h1>
                                 <div className='nae__title-line__link'>
-                                    <a className="link" href="#" itemProp="url" target=""
+                                    <Link
+                                        className="link cta_outline cta_outline__dark hvr-underline-from-right"
+                                        to="#"
+                                        itemProp="url"
+                                        target=""
                                        rel="noopener nofollow noreferrer">
-                                    </a>
+                                        вижте всички
+                                    </Link>
 
                                 </div>
                             </div>
                             <div>
                                 <NewsAndEventsList/>
+                            </div>
+                        </div>
+                    </Container>
+                </div>
+                <div className='nae-container'>
+                    <Container>
+                        <div>
+                            <div className='nae__title-line'>
+                                <h1 className='h1'>
+                                    За нас
+                                </h1>
+                                <div className='nae__title-line__link'>
+                                    <Link
+                                        className="link cta_outline cta_outline__dark hvr-underline-from-right"
+                                        to="#"
+                                        itemProp="url"
+                                        target=""
+                                       rel="noopener nofollow noreferrer">
+                                        вижте всички
+                                    </Link>
+
+                                </div>
+                            </div>
+                            <div>
+
                             </div>
                         </div>
                     </Container>
