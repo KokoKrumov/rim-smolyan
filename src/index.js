ea4898785a7e83bf64e5d25224c913c4e97ed18d
Stiliyan Ivanov <stily.c@gmail.com>
2020-10-05 05:37:28 +0300

    Add logic to init language
    Signed-off-by: Stiliyan Ivanov <stily.c@gmail.com>

--- src/index.js
+++ src/index.js
@@ -16,7 +16,9 @@ const messages = {
     'bg': messages_bg,
     'en': messages_en,
 };
-const language = 'bg';  // language without region code
+const language = localStorage.getItem('lang') ??
+	'bg' ??
+	navigator.languages[0].split(/[-_]/)[0];  // language without region code
 
 ReactDOM.render(
     // <Provider store={store}>
