6316bba86f064ad2322e53a91b6f7225524c0ded
kaloyan.krumov <koko771@abv.bg>
2020-11-18 08:07:33 +0200

    fix missing link, fix strange scroll effect between about us page and regular programs

--- src/components/infoLine/CardInfoLine.js
+++ src/components/infoLine/CardInfoLine.js
@@ -9,12 +9,15 @@ class CardInfoLine extends Component {
 
     state = {
         title: this.props.title,
-        link: this.props.link ? this.props.link : '#'
+        link: this.props.link ? this.props.link : '/#'
     }
 
     handleShowModal(e, data) {
-        e.preventDefault();
-        this.props.showModal(data)
+        if (this.state.link === '/#') {
+            e.preventDefault();
+            this.props.showModal(data)
+        }
+
     }
 
     render() {
@@ -22,29 +25,30 @@ class CardInfoLine extends Component {
         const {intl} = this.props;
         return (
             <div className='info-line info-line__card info-line__bordered__white info-line__titled'>
-                    <div className='info-line__content-wrap'>
-                        <div className='info-line__titled__item'>
-                            <h2 className='h2'
-                               dangerouslySetInnerHTML={{__html: intl.formatMessage({id: this.state.title})}}
+                <div className='info-line__content-wrap'>
+                    <div className='info-line__titled__item'>
+                        <h2 className='h2'
+                            dangerouslySetInnerHTML={{__html: intl.formatMessage({id: this.state.title})}}
+                        >
+
+                        </h2>
+                        <p className='info-line__titled__text info-line__text-bold'>
+                            <a
+
+                                className="link cta_outline cta_outline__red cta_outline__border-overflow"
+                                href={this.state.link}
+                                itemProp="url"
+                                target=""
+                                onClick={(e) => {
+                                    this.handleShowModal(e, 'modal-redirect')
+                                }}
+                                rel="noopener nofollow noreferrer"
+                                dangerouslySetInnerHTML={{__html: intl.formatMessage({id: 'see-here'})}}
                             >
-
-                            </h2>
-                            <p className='info-line__titled__text info-line__text-bold'>
-                                <Link
-
-                                    className="link cta_outline cta_outline__red cta_outline__border-overflow"
-                                    to={this.state.link}
-                                    itemProp="url"
-                                    target=""
-                                    // onClick={(e) => {
-                                    //     this.handleShowModal(e, 'modal-redirect')
-                                    // }} rel="noopener nofollow noreferrer"
-                                    dangerouslySetInnerHTML={{__html: intl.formatMessage({id: 'see-here'})}}
-                                >
-                                </Link>
-                            </p>
-                        </div>
+                            </a>
+                        </p>
                     </div>
+                </div>
             </div>
         )
     }
