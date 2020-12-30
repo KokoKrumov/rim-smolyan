4cd3849eb16091a694ea0ff99d6fa4c949d5a974
Stiliyan Ivanov <stily.c@gmail.com>
2020-12-31 00:28:49 +0200

    Remove modal footer
    Signed-off-by: Stiliyan Ivanov <stily.c@gmail.com>

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -92,10 +92,7 @@ function Footer({showModal}) {
                             </Nav>
                             <Nav className="">
                                 <Nav.Link
-                                    onClick={(e) => {
-                                        handleShowModal('modal-redirect', e)
-                                    }}
-                                    href="#"
+                                    href="/support-us"
                                     className='nav__secondary-link'>
                                     <FormattedMessage id="menu.support"/>
                                 </Nav.Link>
@@ -148,4 +145,3 @@ export default connect(
         showModal
     }
 )(Footer);
-
