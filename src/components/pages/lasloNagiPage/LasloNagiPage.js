862a62122b324fb36a080a1a940527351c789b67
kaloyan.krumov <koko771@abv.bg>
2022-02-27 20:06:48 +0200

    add laslo nagi page

--- src/components/pages/lasloNagiPage/LasloNagiPage.js
+++ src/components/pages/lasloNagiPage/LasloNagiPage.js
@@ -1,344 +1,176 @@
 import React from 'react';
-import aboutUsBg from "../../../assets/images/about-us-page_bg.jpg";
 import Container from "react-bootstrap/cjs/Container";
 import Row from "react-bootstrap/cjs/Row";
 import Col from "react-bootstrap/cjs/Col";
-import AboutInfoLine from "../../infoLine/aboutInfoLine";
+import CardInfoLine from "../../infoLine/CardInfoLine";
+import HeroCollections from "../../hero/HeroCollections";
+import {FormattedMessage, injectIntl} from 'react-intl';
+import {connect} from "react-redux";
+import lasloNagiPageBG from '../../../translations/lasloNagiPageBG.json';
+import lasloNagiPageEN from '../../../translations/lasloNagiPageEN.json';
 import InfoColumn from "../../infoColumn/InfoColumn";
 import CarouselImages from "../../carousel/carouselImages";
-import CardMediaHorizontal from "../../cards/cardMediaHorizontal";
-import CardInfoLine from "../../infoLine/CardInfoLine";
-import {FormattedMessage} from "react-intl";
-import CardTeamHeadmaster from "../../cards/cardTeamHeadmaster";
-import CardTeamMember from "../../cards/cardTeamMember";
+import HeroInner from "../../hero/HeroInner";
+import {OverlayTrigger} from "react-bootstrap";
+import Tooltip from "react-bootstrap/Tooltip";
+import Socials from "../../socials/socials";
 
-function LasloNagiPage(props) {
+
+const LasloNagiPage = (props) => {
+
+	let lasloNagiPageContent = {
+		bgImage: './images/lasloNagi/heroBg.png',
+		aboutHouseBgImage: './images/lasloNagi/aboutHouseBgImage.png',
+		lasloNagiCarouselImg: [
+			{
+				id: 1,
+				image: "../images/lasloNagi/lasloNagiCarouselImg.png"
+			},
+			{
+				id: 2,
+				image: "../images/lasloNagi/lasloNagiCarouselImg.png"
+			},
+			{
+				id: 3,
+				image: "../images/lasloNagi/lasloNagiCarouselImg.png"
+			},
+		]
+	}
+
+	const lasloNagiContent = props.intl.locale === 'en' ? lasloNagiPageEN : lasloNagiPageBG;
+
 	return (
-		<div className='about-us-page'>
-			<div className='hero__wrap hero2cols'>
-				<div
-					className='hero hero-bg'
-					style={{
-						backgroundImage: `url(${aboutUsBg})`
-					}}
-				>
-					<Container>
-						<Row>
-							<Col>
-								<h1 className='h1'>
-									КЪЩА МУЗЕЙ <br/>
-									„ЛАСЛО НАГИ“ – СМОЛЯН
-								</h1>
-							</Col>
-						</Row>
-					</Container>
-				</div>
+		<div className='laslo-nagi__page'>
+			<div className=''>
+				<HeroCollections
+					bgImage={lasloNagiPageContent.bgImage}
+					title={lasloNagiContent.title}
+					label={false}
+				/>
 			</div>
 			<main>
-				<Container className='container position-relative'>
-					<Col>
-						<div className='about-us__info-line__wrap'>
-							{/*<AboutInfoLine/>*/}
-						</div>
-					</Col>
-				</Container>
-				{/*<InfoColumn*/}
-				{/*	title={this.state.infoColumn.history.title}*/}
-				{/*	text={this.state.infoColumn.history.text}*/}
-				{/*	backgroundImage={this.state.infoColumn.history.aboutUsHistoryBg}*/}
-				{/*	showRulesForActivity={this.state.infoColumn.history.showRulesForActivity}*/}
-				{/*	columns={this.state.infoColumn.history.columns}*/}
-				{/*/>*/}
+				<InfoColumn
+					title={lasloNagiContent.aboutHouse}
+					text={lasloNagiContent.aboutHouseText}
+					backgroundImage={lasloNagiPageContent.aboutHouseBgImage}
+					columns={2}
+				/>
 
 				<section className='section-building'>
 					<Container>
 						<Row className='justify-content-between'>
 							<Col lg={5}>
 								<Row>
-									{/*<InfoColumn*/}
-									{/*	title={this.state.infoColumn.building.title}*/}
-									{/*	text={this.state.infoColumn.building.text}*/}
-									{/*	columns={this.state.infoColumn.building.columns}*/}
-									{/*/>*/}
+									<InfoColumn
+										title={lasloNagiContent.permanentExhibitionTitle}
+										text={lasloNagiContent.permanentExhibitionText}
+										columns={1}
+									/>
 								</Row>
 							</Col>
 							<Col lg={6}>
 								<Row>
 									<Col className='pr-0 pl-0'>
-										{/*<div className="about-us-page__building-carousel nae-container">*/}
-										{/*	<CarouselImages*/}
-										{/*		listImages={this.state.listBuildingImagesCarousel}*/}
-										{/*	/>*/}
-										{/*</div>*/}
+										<div className="about-us-page__building-carousel nae-container">
+											<CarouselImages
+												listImages={lasloNagiPageContent.lasloNagiCarouselImg}
+											/>
+										</div>
 									</Col>
 								</Row>
 							</Col>
 						</Row>
-						<Row>
-							<Col>
-								{/*<CardMediaHorizontal*/}
-								{/*	icon={this.state.cardsAccessibility.accessibility.icon}*/}
-								{/*	text={this.state.cardsAccessibility.accessibility.text}*/}
-								{/*/>*/}
-							</Col>
-							<Col>
-								{/*<CardMediaHorizontal*/}
-								{/*	icon={this.state.cardsAccessibility.braille.icon}*/}
-								{/*	text={this.state.cardsAccessibility.braille.text}*/}
-								{/*/>*/}
-							</Col>
-						</Row>
-						<CardInfoLine title={"regular-programs"} link={'/about-us/regular-programs'}/>
 					</Container>
 				</section>
-				<section className='section-team'>
-					{/*<div className='section-team__container-wrap'>*/}
-					{/*	<Container>*/}
-					{/*		<div className="section-team__title__wrap">*/}
-					{/*			<h1*/}
-					{/*				className='h1'*/}
-					{/*			>*/}
-					{/*				<FormattedMessage id="team"/>*/}
-					{/*			</h1>*/}
-					{/*		</div>*/}
-					{/*		<div className='section-team__headmaster-wrap'>*/}
-					{/*			/!*{*!/*/}
-					{/*			/!*	this.props.team.headmaster && this.props.team.headmaster.length !== 0*!/*/}
-					{/*			/!*		?*!/*/}
-					{/*			/!*		this.props.team.headmaster.map(headmaster => {*!/*/}
-					{/*			/!*			return (*!/*/}
-					{/*			/!*				<CardTeamHeadmaster*!/*/}
-					{/*			/!*					key={headmaster.id}*!/*/}
-					{/*			/!*					user={headmaster}*!/*/}
-					{/*			/!*				/>*!/*/}
-					{/*			/!*			)*!/*/}
-					{/*			/!*		})*!/*/}
-					{/*			/!*		:*!/*/}
-					{/*			/!*		<h3 className='h3 loading'>*!/*/}
-					{/*			/!*			...*!/*/}
-					{/*			/!*		</h3>*!/*/}
-					{/*			/!*}*!/*/}
-
-					{/*		</div>*/}
-					{/*	</Container>*/}
-					{/*</div>*/}
-					{/*<div className='section-team__container-wrap'>*/}
-					{/*	<Container>*/}
-					{/*		<div className="section-team__title__wrap">*/}
-					{/*			<h2*/}
-					{/*				className='h2'*/}
-					{/*			>*/}
-					{/*				<FormattedMessage id="department"/>*/}
-					{/*				"<FormattedMessage id="archeology"/>"*/}
-					{/*			</h2>*/}
-					{/*		</div>*/}
-					{/*		<div className='section-team__headmaster-wrap'>*/}
-					{/*			<Row>*/}
-					{/*				{*/}
-					{/*					this.props.team.archeology && this.props.team.archeology.length !== 0*/}
-					{/*						?*/}
-					{/*						this.props.team.archeology.map(user => {*/}
-					{/*							return (*/}
-					{/*								<Col md={6} lg={4} key={user.id} className="section-team__col">*/}
-					{/*									<CardTeamMember*/}
-					{/*										user={user}*/}
-					{/*									/>*/}
-					{/*								</Col>*/}
-					{/*							)*/}
-					{/*						})*/}
-					{/*						:*/}
-					{/*						<h3 className='h3 loading'>*/}
-					{/*							...*/}
-					{/*						</h3>*/}
-					{/*				}*/}
-
-
-					{/*			</Row>*/}
-					{/*		</div>*/}
-					{/*	</Container>*/}
-					{/*</div>*/}
-					{/*<div className='section-team__container-wrap'>*/}
-					{/*	<Container>*/}
-					{/*		<div className="section-team__title__wrap">*/}
-					{/*			<h2*/}
-					{/*				className='h2'*/}
-					{/*			>*/}
-					{/*				<FormattedMessage id="department"/>*/}
-					{/*				"<FormattedMessage id="funds-and-scientific-archives"/>"*/}
-					{/*			</h2>*/}
-					{/*		</div>*/}
-					{/*		<div className='section-team__headmaster-wrap'>*/}
-					{/*			<Row>*/}
-					{/*				{*/}
-					{/*					this.props.team.fundsAndScientificArchives && this.props.team.fundsAndScientificArchives.length !== 0*/}
-					{/*						?*/}
-					{/*						this.props.team.fundsAndScientificArchives.map(user => {*/}
-					{/*							return (*/}
-					{/*								<Col md={6} lg={4} key={user.id} className="section-team__col">*/}
-					{/*									<CardTeamMember*/}
-					{/*										user={user}*/}
-					{/*									/>*/}
-					{/*								</Col>*/}
-					{/*							)*/}
-					{/*						})*/}
-					{/*						:*/}
-					{/*						<h3 className='h3 loading'>*/}
-					{/*							...*/}
-					{/*						</h3>*/}
-					{/*				}*/}
-
-
-					{/*			</Row>*/}
-					{/*		</div>*/}
-					{/*	</Container>*/}
-					{/*</div>*/}
-
-
-					{/*{*/}
-					{/*	this.props.team.history && this.props.team.history.length !== 0*/}
-					{/*		?*/}
-					{/*		<div className='section-team__container-wrap'>*/}
-					{/*			<Container>*/}
-					{/*				<div className="section-team__title__wrap">*/}
-					{/*					<h2*/}
-					{/*						className='h2'*/}
-					{/*					>*/}
-					{/*						<FormattedMessage id="department"/>*/}
-					{/*						"<FormattedMessage id="history"/>"*/}
-					{/*					</h2>*/}
-					{/*				</div>*/}
-					{/*				<div className='section-team__headmaster-wrap'>*/}
-					{/*					<Row>*/}
-					{/*						{this.props.team.history.map(user => {*/}
-					{/*							return (*/}
-					{/*								<Col md={6} lg={4} key={user.id} className="section-team__col">*/}
-					{/*									<CardTeamMember*/}
-					{/*										user={user}*/}
-					{/*									/>*/}
-					{/*								</Col>*/}
-					{/*							)*/}
-					{/*						})}*/}
-
-
-					{/*					</Row>*/}
-					{/*				</div>*/}
-					{/*			</Container>*/}
-					{/*		</div>*/}
-					{/*		:*/}
-					{/*		null*/}
-					{/*}*/}
-
-					{/*{*/}
-					{/*	this.props.team.ethnography && this.props.team.ethnography.length !== 0*/}
-					{/*		?*/}
-					{/*		<div className='section-team__container-wrap'>*/}
-					{/*			<Container>*/}
-					{/*				<div className="section-team__title__wrap">*/}
-					{/*					<h2*/}
-					{/*						className='h2'*/}
-					{/*					>*/}
-					{/*						<FormattedMessage id="department"/>*/}
-					{/*						"<FormattedMessage id="ethnography"/>"*/}
-					{/*					</h2>*/}
-					{/*				</div>*/}
-					{/*				<div className='section-team__headmaster-wrap'>*/}
-					{/*					<Row>*/}
-					{/*						{*/}
-					{/*							this.props.team.ethnography.map(user => {*/}
-					{/*								return (*/}
-					{/*									<Col md={6} lg={4} key={user.id}*/}
-					{/*										 className="section-team__col">*/}
-					{/*										<CardTeamMember*/}
-					{/*											user={user}*/}
-					{/*										/>*/}
-					{/*									</Col>*/}
-					{/*								)*/}
-					{/*							})*/}
-					{/*						}*/}
-
-
-					{/*					</Row>*/}
-					{/*				</div>*/}
-					{/*			</Container>*/}
-					{/*		</div>*/}
-					{/*		:*/}
-					{/*		null*/}
-					{/*}*/}
-
-					{/*<div className='section-team__container-wrap'>*/}
-					{/*	<Container>*/}
-					{/*		<div className="section-team__title__wrap">*/}
-					{/*			<h2*/}
-					{/*				className='h2'*/}
-					{/*			>*/}
-					{/*				<FormattedMessage id="department"/>*/}
-					{/*				"<FormattedMessage id="public-relations"/>"*/}
-					{/*			</h2>*/}
-					{/*		</div>*/}
-					{/*		<div className='section-team__headmaster-wrap'>*/}
-					{/*			<Row>*/}
-					{/*				{*/}
-					{/*					this.props.team.publicRelations && this.props.team.publicRelations.length !== 0*/}
-					{/*						?*/}
-					{/*						this.props.team.publicRelations.map(user => {*/}
-					{/*							return (*/}
-					{/*								<Col md={6} lg={4} key={user.id} className="section-team__col">*/}
-					{/*									<CardTeamMember*/}
-					{/*										user={user}*/}
-					{/*									/>*/}
-					{/*								</Col>*/}
-					{/*							)*/}
-					{/*						})*/}
-					{/*						:*/}
-					{/*						<h3 className='h3 loading'>*/}
-					{/*							...*/}
-					{/*						</h3>*/}
-					{/*				}*/}
-
-
-					{/*			</Row>*/}
-					{/*		</div>*/}
-					{/*	</Container>*/}
-					{/*</div>*/}
-					{/*<div className='section-team__container-wrap'>*/}
-					{/*	<Container>*/}
-					{/*		<div className="section-team__title__wrap">*/}
-					{/*			<h2*/}
-					{/*				className='h2'*/}
-					{/*			>*/}
-					{/*				<FormattedMessage id="department"/>*/}
-					{/*				"<FormattedMessage id="administration-and-financial"/>"*/}
-					{/*			</h2>*/}
-					{/*		</div>*/}
-					{/*		<div className='section-team__headmaster-wrap'>*/}
-					{/*			<Row>*/}
-					{/*				{*/}
-					{/*					this.props.team.administration && this.props.team.administration.length !== 0*/}
-					{/*						?*/}
-					{/*						this.props.team.administration.map(user => {*/}
-					{/*							return (*/}
-					{/*								<Col md={6} lg={4} key={user.id} className="section-team__col">*/}
-					{/*									<CardTeamMember*/}
-					{/*										user={user}*/}
-					{/*									/>*/}
-					{/*								</Col>*/}
-					{/*							)*/}
-					{/*						})*/}
-					{/*						:*/}
-					{/*						<h3 className='h3 loading'>*/}
-					{/*							...*/}
-					{/*						</h3>*/}
-					{/*				}*/}
-
-
-					{/*			</Row>*/}
-					{/*		</div>*/}
-					{/*	</Container>*/}
-					{/*</div>*/}
+				<section className='section-building'>
+					<HeroInner
+						title={lasloNagiContent.priceAndWorkingHours}
+						subtitle={''}
+					/>
+					<div className='contacts contacts__wrap laslo-nagi__page__contact-section'>
+						<Container className=''>
+							<Row className=''>
+								<Col lg={11}>
+									<Row className={'contacts__row'}>
+										<Col lg={5}>
+											<div>
+												<p className={'title'}>
+													<b>Ценоразпис на входни билети и беседи в
+														къща музей „Ласло Наги“</b>
+												</p>
+												<ul type={'decimal'} className={'list'}>
+													<li>
+														1. Входни такси:
+														<ul className={'list'}>
+															<li>
+																- възрастни – 3.00 лв.
+															</li>
+															<li>
+																- учащи и пенсионери – 1.00 лв.
+															</li>
+															<li>
+																- деца до 7 години – безплатно
+															</li>
+															<li>
+																- учащи и пенсионери над 10 човека – 1.00 лв.
+															</li>
+														</ul>
+													</li>
+													<li>
+														2. Беседи:
+														<ul className={'list'}>
+															<li>
+																- на български език – 10.00 лв.
+															</li>
+															<li>
+																- беседи на чужд език се изнасят само след предварителна заявка. Цена - 20.00 лв.
+															</li>
+														</ul>
+													</li>
+												</ul>
+											</div>
+										</Col>
+										<Col lg={3}>
+											<ul className='address__list'>
+												<li className='address__list-item'>
+													<h4 className='h4 contacts__address-label'>
+														Работно време:
+													</h4>
+													<p className='contacts__address-text'>
+														Вторник - Събота
+													</p>
+												</li>
+												<li className='address__list-item time-period__wrap'>
+													<h6 className='time-period__title'>
+														Май - Септември
+													</h6>
+													<p className='time-period__text'>
+														<b>10:00 - 18:00</b>
+													</p>
+													<hr/>
+													<h6 className='time-period__title'>
+														Октомври - Април
+													</h6>
+													<p className='time-period__text'>
+														<b>10:00 - 17:00</b>
+													</p>
+												</li>
+												<li className='address__list-item'>
+													<p className='paragraph-3'>
+														Неделя и понеделник са почивни дни.
+													</p>
+												</li>
+											</ul>
+										</Col>
+									</Row>
+								</Col>
+							</Row>
+						</Container>
+					</div>
 				</section>
 			</main>
 		</div>
 	)
 }
 
-export default LasloNagiPage;
+export default injectIntl(LasloNagiPage);
