fb97f514b82f8b564d15909fad87c6cffaff8090
Stiliyan Ivanov <stily.c@gmail.com>
2026-01-28 18:07:43 +0200

    Add security headers (#108)
    Signed-off-by: Stiliyan Ivanov <stily.c@gmail.com>

--- public/router.php
+++ public/router.php
@@ -22,6 +22,15 @@ $path_beginings = [
 
 $path_begining = '/' . explode('/', $_SERVER['REQUEST_URI'])[1];
 
+// Security headers
+header("Content-Security-Policy: frame-ancestors 'self'");
+header("Strict-Transport-Security: max-age=63072000; includeSubdomains; preload");
+header("X-Frame-Options: SAMEORIGIN");
+header("X-XSS-Protection: 1; mode=block");
+header("X-Content-Type-Options: nosniff");
+header("Referrer-Policy: strict-origin-when-cross-origin");
+header("Cache-Control: must-revalidate, proxy-revalidate");
+
 if ( ! in_array($path_begining, $path_beginings)) {
     http_response_code(404);
 }
